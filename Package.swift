00cb0f80baf1845290fbed7853b03d7075bb1cb5
ci <ci@banuba.com>
2023-11-24 17:07:10 +0300

    1.9.1-86-g65c1c833d4

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.9.1-86-g65c1c833d4"
+
+let package = Package(
+    name: "BNBSdkApi",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBSdkApi",
+            targets: [
+                "BNBSdkApi",
+                "BNBSdkApi_BNBSdkCore"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/sdk-banuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBSdkApi",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-86-g65c1c833d4/BNBSdkApi.zip",
+            checksum: "a084274804b8caea7c7b5c248458b5b7ea6b6e4812e24bb6345dbf8b3406e979"
+        ),
+        .target(
+            name: "BNBSdkApi_BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+    ]
+)
