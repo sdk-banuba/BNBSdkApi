756f19a00da79e06833bb99d1ea4671786af3abe
ci <ci@banuba.com>
2024-09-05 11:54:01 +0300

    1.15.2

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.1-106-g81956e5c7"
+let bnbPackageVersion: Version = "1.15.2"
 
 let package = Package(
     name: "BNBSdkApi",
@@ -26,8 +26,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBSdkApi",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-106-g81956e5c7/BNBSdkApi.zip",
-            checksum: "a22881df29cdf0131aa1f733261ef0bd89e2dcd683d460d26ba1bb66729b3aa9"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.15.2/BNBSdkApi.zip",
+            checksum: "b527806f958e4a1012e637215f72243144e3366d40046f247de733e5fe811ada"
         ),
         .target(
             name: "BNBSdkApi_BNBSdkCore",
